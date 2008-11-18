7cb22c9c6717f190c2299bf9532e898f76f1e618
oicawa <oicawa@1d3d8b80-664f-4c7c-9890-4cefb2de5291>
2008-11-18 08:35:00 +0000

    git-svn-id: https://svn.osdn.net/svnroot/cria@61 1d3d8b80-664f-4c7c-9890-4cefb2de5291

--- Cria/Hello.cr
+++ Cria/Hello.cr
@@ -1,3 +1,12 @@
-if true
-    write("if statement is success!\n")
+write("Welcome to cria world.\n" + "What's your name? >> ")
+name = read()
 
+write("Are you male? >> ")
+answer = read()
+
+if false
+    write("Nice to meet you, Mr.%s\n\n", name)
+else
+    write("Nice to meet you, Miss,%s\n\n", name)
+
+write("Good bye.\n")
