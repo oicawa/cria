26303031e5f2835e8b32cb00bef729bb0c741cbb
oicawa <oicawa@1d3d8b80-664f-4c7c-9890-4cefb2de5291>
2008-12-23 14:07:49 +0000

    git-svn-id: https://svn.osdn.net/svnroot/cria@123 1d3d8b80-664f-4c7c-9890-4cefb2de5291

--- Cria/Person.cr
+++ Cria/Person.cr
@@ -21,10 +21,22 @@ Person:
 
 person = Person()
 
-write("Input your first name. >> ")
+write("Input your first name.  >> ")
 person.first_name = read()
 
 write("Input your family name. >> ")
 person.family_name = read()
 
+write("Input your sex.         >> ")
+person.sex = read()
+
+write("Input your age.         >> ")
+person.age = read()
+
+write("Input your birthday.    >> ")
+person.birthday = read()
+
 write("%s-%s\n", person.first_name, person.family_name)
+write("  sex     : %s\n", person.sex)
+write("  age     : %s\n", person.age)
+write("  birthday: %s\n", person.birthday)
