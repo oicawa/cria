0ca2ebf75c5b320d2e76bfcbabcaf39158e142b3
oicawa <oicawa@1d3d8b80-664f-4c7c-9890-4cefb2de5291>
2009-01-10 14:11:56 +0000

    git-svn-id: https://svn.osdn.net/svnroot/cria@132 1d3d8b80-664f-4c7c-9890-4cefb2de5291

--- Cria/Person.cr
+++ Cria/Person.cr
@@ -18,53 +18,24 @@ Person:
         return honorific + .family_name
 
 
-person = Person()
-
-write("Input your first name.  >> ")
-person.first_name = read()
-
-write("Input your family name. >> ")
-person.family_name = read()
-
-write("Are you male? (y/n)     >> ")
-answer = read()
-if answer == "y"
-    person.sex = true
-else
-    person.sex = false
-
-write("Have you married.(y/n)  >> ")
-answer = read()
-if answer == "y"
-    person.has_married = true
-else
-    person.has_married = false
-
-#write("Input your birthday.    >> ")
-#person.birthday = read()
-
-#person.hogehoge = "fugafuga"
-write("Family Name is member field? (family_name='%s')\n", person.family_name)
-#write("Family Name is global variable? (family_name='%s')\n", family_name)
-write("Hello, %s!\n", person.get_honorific_name())
-#write("%s-%s\n", person.first_name, person.family_name)
-#write("  sex     : %s\n", person.sex)
-#write("  age     : %s\n", person.age)
-#write("  birthday: %s\n", person.birthday)
-
+oikawa = Person()
+oikawa.first_name = "Masamitsu"
+oikawa.family_name = "Oikawa"
+oikawa.sex = true
+oikawa.has_married = true
 
 watanabe = Person()
 watanabe.first_name = "Fumie"
 watanabe.family_name = "Watanabe"
 watanabe.sex = false
 watanabe.has_married = false
-write("Your honny is %s\n", watanabe.get_honorific_name())
-#write("Your honny is %s\n", watanabe.family_name)
-#write("Your name is %s\n", person.family_name)
 
-wife = Person()
-wife.first_name = "Fumie"
-wife.family_name = "Oikawa"
-wife.sex = false
-wife.has_married = true
-write("Your wife is %s\n", wife.get_honorific_name())
+fumie = Person()
+fumie.first_name = "Fumie"
+fumie.family_name = "Oikawa"
+fumie.sex = false
+fumie.has_married = true
+
+write("Your name is %s\n", oikawa.get_honorific_name())
+write("Your honny is %s\n", watanabe.get_honorific_name())
+write("Your wife is %s\n", fumie.get_honorific_name())
--- Cria/_Hash.c
+++ Cria/_Hash.c
@@ -1,45 +1,26 @@
-typedef HashEntryTag    *Entry;
-typedef HashTypeTag     *HashType;
-typedef HashTag         *Hash;
+#include <memory.h>
 
-struct HashTag
-{
-    HashType    type;
-    int         binsCount;
-    int         entriesCount;
-    HashEntry*  bins;
-};
+#include "../Memory/Memory.h"
+#include "../Logger/Logger.h"
 
+#include "String.h"
 
-struct HashTypeTag
-{
-    int (*compare)();   //比較関数
-    int (*hash)();      //ハッシュ関数
-};
-
-
-struct HashEntryTag
-{
-    unsigned int    value;
-    string          key;
-    string          record;
-    HashEntry       next;
-};
+#include "_Hash.h"
 
 
 Hash
-hash_new(
-    int         size,
-    HashType    type;
+Hash_new(
+    int size
 )
 {
     Logger_trc("[ START ]%s", __func__);
     Hash hash = Memory_malloc(sizeof(struct HashTag));
     memset(hash, 0x00, sizeof(struct HashTag));
-    hash->type = type;
-    hash->binsCount = size;
-    hash->entriesCount = 0;
-    hash->bins = NULL;
+    hash->size = size;
+    hash->count = 0;
+    Entry* entrys = Memory_malloc(size);
+    memset(entrys, 0x00, size);
+    hash->entrys = entrys;
     
     Logger_trc("[  END  ]%s", __func__);
     return hash;
@@ -48,17 +29,97 @@ hash_new(
 
 
 unsigned int 
-hash_getKey(
-    char *s
+Hash_get_key_value(
+	int size,
+    char* key
 )
 {
-    unsigned int hash = 0;
-    for (;*s != '\0'; s++)
+    int hash_value = 0;
+    int c;
+    
+    while ((c = *key) != '\0')
     {
-        hash = hash * 137 + *s;
+        hash_value += (int)c;
+        key++;
     }
     
-    return h % 1987;
+    return (hash_value % size);
+}
+
+
+
+Entry
+Hash_get_entry(
+	Hash hash,
+	int hash_value,
+	char* key
+)
+{
+	Entry entry = NULL;
+	
+	return entry;
+}
+
+
+
+void
+Hash_put(
+	Hash hash,
+	char* key,
+	void* object
+)
+{
+    Logger_trc("[ START ]%s", __func__);
+    int hash_value;
+    Entry entry;
+    Entry cursor;
+    
+    
+    hash_value = Hash_get_key_value(hash->size, key);
+
+
+    entry = Hash_get_entry(hash, hash_value, key);
+    if (entry != NULL)
+		goto END;
+
+
+    entry = Memory_malloc(sizeof(struct EntryTag));
+    memset(entry, 0x00, sizeof(struct EntryTag));
+    entry->key = String_clone(key);
+    entry->hash_value = hash_value;
+    entry->next = NULL;
+
+
+	cursor = (Entry)((hash->entrys)[hash_value]);
+	if (cursor == NULL)
+	{
+		(hash->entrys)[hash_value] = entry;
+		goto END;
+	}
+	
+	while (cursor->next != NULL)
+		cursor = cursor->next;
+	
+	cursor->next = entry;
+
+END:
+	entry->object = object;
+	
+    Logger_trc("[  END  ]%s", __func__);
+    return;
+}
+
+
+
+void*
+Hash_get(
+	Hash hash,
+	char* key
+)
+{
+	void* object = NULL;
+	
+	return object;
 }
 
 
