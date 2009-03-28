640c3807e141f0831eaa55379360b7e89ebc7a5d
oicawa <oicawa@1d3d8b80-664f-4c7c-9890-4cefb2de5291>
2009-03-28 11:23:16 +0000

    git-svn-id: https://svn.osdn.net/svnroot/cria@193 1d3d8b80-664f-4c7c-9890-4cefb2de5291

--- Cria/_Loader.c
+++ Cria/_Loader.c
@@ -120,16 +120,67 @@ Loader_load(
 {
     Logger_trc("[ START ]%s", __func__);
     char buffer[MAX_PATH_LENGTH];
+    StringBuffer cria_home_path = StringBuffer_new();
+    StringBuffer current_path = StringBuffer_new();
     
-    memset(buffer, 0x00, MAX_PATH_LENGTH);
     
     //カレントディレクトリのパスを取得
-    //getcwd関数を使用
+    memset(buffer, 0x00, MAX_PATH_LENGTH);
+    getcwd(buffer, MAX_PATH_LENGTH);
+    StringBuffer_append(current_path, buffer);
+    StringBuffer_append(current_path, "/");
+    StringBuffer_append(current_path, loader->library_name);
+    printf("*** [%s] ***\n", StringBuffer_toString(current_path));
+    
     
     //実行ファイルが存在するパスを取得
-    //#include <unistd.h>
+    //（※これで実行ファイルのフルパスは取得できるので、
+    //　　あとはこのフルパスからディレクトリパスだけ取得すればいい。）
+    memset(buffer, 0x00, MAX_PATH_LENGTH);
     readlink("/proc/self/exe", buffer, MAX_PATH_LENGTH);
-    printf("***** [%s] *****\n\n\n", buffer);
+    
+
+    //ここにディレクトリパスだけ取得する処理を。。
+    size_t length = strlen(buffer);
+    char* pointer = strrchr(buffer, '/');
+    size_t index = pointer - buffer;
+    memset(pointer + 1, 0x00, length - index);
+    
+    
+    StringBuffer_append(cria_home_path, buffer);
+    StringBuffer_append(cria_home_path, loader->library_name);
+    printf("*** [%s] ***\n", StringBuffer_toString(cria_home_path));
+    
+    
+    //★問題発生★
+    //$PATHに、criaの実行ファイルのありかを追加して、
+    //任意のディレクトリからcriaを実行しようとすると、
+    //libcria.so共有ライブラリのリンクに失敗する。
+    //で、これをcriaを実行した時のカレントディレクトリに置くと正常に実行ができる。
+    //どうやらコンパイル時の-rpathオプションは、
+    //実行時の「カレントディレクトリ」に対するライブラリのありかを指定しているらしい。
+    //実行時に「実行ファイルと同じディレクトリ」にあるライブラリをリンクさせるオプションはないものか。。
+    //
+    //！！あったっぽい！！
+    //ldコマンドに、「-Wl,-rpath,$ORIGIN」つーオプションを指定するとまさにやりたいことができるらしい。
+    //んが、試してみたらダメだった。指定するのはリンクのときだよな〜。なんでダメなんだろう。。
+    //「-z origin」つーオプションもあるみたいなので試してみたけどダメだった。
+    //もちっとネットで調査が必要かな。
+    //
+    //もしないようなら、現在libcria.soとしているライブラリも、
+    //実行時にdlopenとかdlclose使ってダイナミックにロードするしかないなぁ。
+    //でも絶対オプションでできるはず。。
+    //
+    //このこだわりはcriaのコンセプトとしては非常に重要なのです。
+    //インストールしてアーカイブを展開したらそのまま使えるというのが。
+    //環境変数の設定したくないし。
+    //あ、でもPATHのどっかにcria実行ファイルへのリンクを作る必要があるか。。
+    
+    
+    //ライブラリの存在チェック。
+    
+    //dlopen(
+    
     
     
     
