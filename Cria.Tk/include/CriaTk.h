17ec2dfd84cfd6afd4db7978a63029732df66682
oicawa <oicawa@1d3d8b80-664f-4c7c-9890-4cefb2de5291>
2009-04-30 06:52:06 +0000

    git-svn-id: https://svn.osdn.net/svnroot/cria@270 1d3d8b80-664f-4c7c-9890-4cefb2de5291

---
+++ Cria.Tk/include/CriaTk.h
@@ -0,0 +1,20 @@
+#ifndef PUBLIC_CRIA_TK_H_INCLUDED
+#define PUBLIC_CRIA_TK_H_INCLUDED
+
+
+#include "CriaId.h"
+#include "CriaObject.h"
+#include "List.h"
+#include "Interpreter.h"
+#include "Definition.h"
+
+
+
+DefinitionClass
+CriaTk_loadClass(
+    String className
+);
+
+
+
+#endif
