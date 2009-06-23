a4ab56033f01a438be4c9e351738108fdc7acc94
oicawa <oicawa@1d3d8b80-664f-4c7c-9890-4cefb2de5291>
2009-06-23 13:40:46 +0000

    git-svn-id: https://svn.osdn.net/svnroot/cria@294 1d3d8b80-664f-4c7c-9890-4cefb2de5291

---
+++ Cria/include/CriaBlock.h
@@ -0,0 +1,49 @@
+#ifndef PUBLIC_CRIABLOCK_H_INCLUDED
+#define PUBLIC_CRIABLOCK_H_INCLUDED
+
+
+
+#include "CriaId.h"
+#include "List.h"
+#include "Definition.h"
+#include "Expression.h"
+#include "Interpreter.h"
+
+
+typedef struct CriaBlockTag *CriaBlock;
+
+
+
+struct CriaBlockTag
+{
+    struct CriaIdTag id;
+    Interpreter interpreter;
+    CriaId object;
+    List parameterList;
+    List parameters;
+    DefinitionFunction function;
+    CriaId parent;
+};
+
+
+
+CriaBlock
+CriaBlock_new(
+    Interpreter interpreter,
+    CriaId object,
+    List parameterList,
+    List parameters,
+    DefinitionFunction function,
+    CriaId parent
+);
+
+
+
+CriaId
+CriaBlock_evaluate(
+    CriaBlock block
+);
+
+
+
+#endif
