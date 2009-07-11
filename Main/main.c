fd4a02a4db84b2e8a95d8d92c94983362117d433
oicawa <oicawa@1d3d8b80-664f-4c7c-9890-4cefb2de5291>
2009-07-11 09:54:12 +0000

    git-svn-id: https://svn.osdn.net/svnroot/cria@310 1d3d8b80-664f-4c7c-9890-4cefb2de5291

--- Main/main.c
+++ Main/main.c
@@ -1,43 +1,29 @@
 #include <stdio.h>
-#include <memory.h>
 
 #include "Interpreter.h"
+#include "Logger.h"
 
-#define BUFFER_SIZE 80
+
 
 //========================================
 //Main
 //========================================
 int
 main(
-    int argc,
-    char** argv
+    int     argc,
+    char**  argv
 )
 {
     int endCode = 0;
     Interpreter interpreter = NULL;
-    FILE* file = NULL;
-    Boolean is_stdout = TRUE;
-    char buffer[BUFFER_SIZE + 1]; 
-    char* lf = NULL;
-    int line = 1;
-    String file_path = NULL;
+    
+    //Logger_set(LOG_LEVEL_DEBUG, "Loader_parse");
+    //Logger_set(LOG_LEVEL_TRACE, NULL);
+
     
     if (argv[1] == NULL)
     {
-        file = stdin;
-        file_path = String_new("<<stdin>>");
-    }
-    else
-    {
-        file = fopen(argv[1], "r");
-        file_path = String_new(argv[1]);
-        is_stdout = FALSE;
-    }
-    
-    if (file == NULL)
-    {
-        fprintf(stderr, "File open error. ('%s')\n", file_path);
+        printf("Input cria script file path.\n");
         goto END;
     }
     
@@ -49,30 +35,15 @@ main(
         goto END;
     }
     
-    while (feof(file) == 0)
-    {
-        memset(buffer, 0x00, sizeof(buffer));
-        lf = NULL;
-        
-        fgets(buffer, sizeof(buffer) - sizeof(char), file);
-        lf = strchr(buffer, '\n'); 
-        if (lf != NULL)
-        {
-            *lf = '\0';
-        }
-        else if (feof(file) == FALSE)
-        {
-            fprintf(stderr, "[%s] line %3d : Too long sentence. Please get the line under %d columns.\n\n", argv[1], line, BUFFER_SIZE);
-            goto END;
-        }
-        
-        //TODO: I have to implement logic for that 1 line is over buffer size.
-        Interpreter_run(interpreter, file_path, line, buffer);
-        
-        line++;
-    }
     
-	
+    Interpreter_compile(interpreter, argv[1]);
+    
+    
+    Interpreter_run(interpreter);
+    
+    
+    //TODO: I have to improve that this function called, when the process is killed.
+    //      Use signal?
     Interpreter_dispose(interpreter);
     
     
@@ -80,8 +51,6 @@ main(
     
 END:
     interpreter = NULL;
-    if (is_stdout == TRUE)
-        fclose(file);
     
     return endCode;
 }
